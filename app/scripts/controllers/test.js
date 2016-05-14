c9a23914f4979c820c5da51bc1e882f6e1b319d6
cckn1ght <qunhaosong@gmail.com>
2016-05-14 00:15:11 -0500

    webapp0.0.1

---
+++ app/scripts/controllers/test.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name webApp001App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the webApp001App
+ */
+angular.module('webApp001App')
+  .controller('TestCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
